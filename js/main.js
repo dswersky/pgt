8f358b2390ccd1dd525fa100f1a500c6aaecd571
dswersky <dswersky@gmail.com>
2015-01-10 16:27:06 -0500

    inital commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var app = {
+
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            $('body').html(new HomeView(self.store).render().el);
+        });
+        //$('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    showAlert: function(message, title) {
+        if (navigator.notification){
+            navigator.notification.alert(message, null, title, 'OK');
+        }
+        else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+};
+
+app.initialize();
