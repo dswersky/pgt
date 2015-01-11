7981960a4787722f5a0c3b45b2e8e00ba8a3626f
dswersky <dswersky@gmail.com>
2015-01-11 13:52:30 -0500

    Added geolocation

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -1,6 +1,7 @@
 var EmployeeView= function(employee) {
 	this.initialize = function() {
 		this.el = $('<div/>');
+		this.el.on('click', '.add-location-btn', this.addLocation);
 	};
 
 	this.render = function() {
@@ -8,6 +9,17 @@ var EmployeeView= function(employee) {
 		return this;
 	};
 
+	this.addLocation = function(event) {
+		event.preventDefault();
+		console.log('addLocation');
+		navigator.geolocation.getCurrentPosition(
+			function(position) {
+				$('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+			},
+			function() {
+				alert('Error getting location');
+			});
+	}
 	this.initialize();
 }
 
